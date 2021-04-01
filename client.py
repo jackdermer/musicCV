2cca08bc35c1dbf02473ffa25d2e133c666c2b20
jackdermer <55258108+jackdermer@users.noreply.github.com>
2021-04-01 19:57:02 -0400

    music time

--- client.py
+++ client.py
@@ -22,5 +22,23 @@ def update_vars():
         c1 = cams[1]
         c2 = cams[2]
         c3 = cams[3]
+        print(cams)
+
+audio = Server().boot()
+audio.start()
+
+length = 1
+scale = 3
+f = Adsr(attack=length, decay=length, sustain=0, release=0)
+
+while True:
+    update_vars()
+    f.play()
+    a = Sine(freq=c0*scale, mul=f).out(0)
+    b = Sine(freq=c1*scale, mul=f).out(0)
+    c = Sine(freq=c2*scale, mul=f).out(1)
+    d = Sine(freq=c3*scale, mul=f).out(1)
+
+    time.sleep(length*2)
     
 s.close()
