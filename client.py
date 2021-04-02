91fe0c625e52beb506d654982284d0f1c6ce3373
jackdermer <55258108+jackdermer@users.noreply.github.com>
2021-04-01 22:18:59 -0400

    music time

--- client.py
+++ client.py
@@ -12,7 +12,7 @@ s.connect((HOST, PORT))
 c0 = 0
 c1 = 0
 c2 = 0
-c3 = 0
+# c3 = 0
 
 def update_vars():
     data = s.recv(1024)
@@ -21,33 +21,33 @@ def update_vars():
         global c0
         global c1
         global c2
-        global c3
+        # global c3
         c0 = cams[0]
         c1 = cams[1]
         c2 = cams[2]
-        c3 = cams[3]
-        print(cams)
+        # c3 = cams[3]
+        print(cams[0:3])
 
 audio = Server().boot()
 audio.start()
 
-length = 0.5
+length = 0.8
 scale = 3
 f = Adsr(attack=length, decay=length, sustain=0, release=0)
 
 while True:
     update_vars()
     f.play()
-    a = Sine(freq=c0, mul=f).out(0)
+    a = Sine(freq=c0, mul=f).out()
     time.sleep(length*2)
     f.play()
-    b = Sine(freq=c1, mul=f).out(0)
+    b = Sine(freq=c1, mul=f).out()
     time.sleep(length*2)
     f.play()
-    c = Sine(freq=c2, mul=f).out(0)
+    c = Sine(freq=c2, mul=f).out()
     time.sleep(length*2)
-    f.play()
-    d = Sine(freq=c3, mul=f).out(0)
-    time.sleep(length*2)
+    # f.play()
+    # d = Sine(freq=c3, mul=f).out(0)
+    # time.sleep(length*2)
     
 s.close()
